b6712d2fc4b125770de5758d6535c34fecf2c640
LoboMenezes <41973862+LoboMenezes@users.noreply.github.com>
2019-05-13 19:48:11 -0300

    Add files via upload

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -3,12 +3,7 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { TwdServiceProvider } from "../../providers/twd-service/twd-service";
 import { Episode } from "../../app/models/episode";
 
-/**
- * Generated class for the DetailsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
