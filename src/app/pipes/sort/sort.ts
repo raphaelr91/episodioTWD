80dcc2d84168ba7e095c595292d0fc026e66dc65
LoboMenezes <41973862+LoboMenezes@users.noreply.github.com>
2019-05-13 19:50:23 -0300

    Add files via upload

--- src/app/pipes/sort/sort.ts
+++ src/app/pipes/sort/sort.ts
@@ -1,17 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-/**
- * Generated class for the SortPipe pipe.
- *
- * See https://angular.io/api/core/Pipe for more info on Angular Pipes.
- */
+
 @Pipe({
   name: 'sort',
 })
 export class SortPipe implements PipeTransform {
-  /**
-   * Takes a value and makes it lowercase.
-   */
+  
   transform(array: Array<string>, args?: any): Array<string> {
     return array.sort(function(a, b){
       if(a[args.property] < b[args.property]){
